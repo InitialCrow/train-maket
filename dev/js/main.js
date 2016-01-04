02059d425804cc1ccbf9c823ecc1fc22cc7b561c
Adam <parent.adam@free.fr>
2016-01-04 17:02:55 +0100

    beautiful 0.1

---
+++ dev/js/main.js
@@ -0,0 +1,11 @@
+//main.js
+//
+$(document).ready(function(){
+	  var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      speed: 400,
+      spaceBetween: 100
+    }) ; 
+});
